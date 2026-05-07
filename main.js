5a350555cc8a662e20623eb055b227689079f7de
owng742 <owng@ucsc.edu>
2026-05-07 13:06:37 -0700

    organized grass tiles and tested loading them

--- main.js
+++ main.js
@@ -10,21 +10,33 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+let grassTiles = []
 
 function preload ()
 {
-    this.load.image('temp', './PNG/mapTile_001.png')
-    this.load.image('water', './PNG/mapTile_188.png')
+    for(let i = 0; i <= 12; i++){
+        const name = `grass${i}`;
+        grassTiles.push(name);
+        this.load.image(name, `./PNG/grass_tiles/grass${i}.png`);
+    }
+
+    this.load.image('water', './PNG/water3.png')
 }
 
 function create ()
 {
-    for(let x  = 1; x <= 20; x++){
+    // Fill entire grid with water
+    for(let x = 1; x <= 20; x++){
         for(let y = 1; y <= 15; y++){
             this.add.image((x * 64) - 32, (y * 64) - 32, 'water')
         }
     }
-    
+    // Load grass sprites
+    let index = 0
+    for(let x = 1; x <= 13; x++){
+        this.add.image((x * 64) + 32, 96, grassTiles[index])
+        index++;
+    }
 
 }
 
