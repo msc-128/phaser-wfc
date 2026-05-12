a32cc4596255563e5b80c1d202f8b0f8f4b8772c
owng742 <owng@ucsc.edu>
2026-05-12 11:31:50 -0700

    added different terrains and decor

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ function propagate(x, y){
         if(!this.grid[nx]?.[ny]) continue;
 
         this.grid[nx][ny] = this.grid[nx][ny].filter(options => {
-            let index = parseInt(options.replace("grass", ""));
+            let index = parseInt(options);
             return rules[n.dir].includes(index);
         })
     }
@@ -82,21 +82,23 @@ function decorate(){
     for(let x = 0; x < GRID_COLS; x++) {
         for(let y = 0; y < GRID_ROWS; y++) {
             console.log(this.grid[x][y][0]);
-            if (this.grid[x][y][0] != "grass16" && this.grid[x][y][0] != "grass15" &&
-                this.grid[x][y][0] != "grass14" && this.grid[x][y][0] != "grass13" &&
-                this.grid[x][y][0] != "grass12" && this.grid[x][y][0] != "grass11" &&
-                this.grid[x][y][0] != "grass10") {
+            if (this.grid[x][y][0] != 16 && this.grid[x][y][0] != 15 &&
+                this.grid[x][y][0] != 14 && this.grid[x][y][0] != 13 &&
+                this.grid[x][y][0] != 12 && this.grid[x][y][0] != 11 &&
+                this.grid[x][y][0] != 10) {
                 let r = Math.random();
                 let decorX = ((x+1) * TILE_SIZE) - TILE_SIZE/2;
                 let decorY = ((y+1) * TILE_SIZE) - TILE_SIZE/2;
-                if (r < 0.7) {
+                if (r < 0.6) {
                     continue;
+                } else if (r < 0.7) {
+                    this.add.image(decorX, decorY, `${terrain}_dec0`)
                 } else if (r < 0.8) {
-                    this.add.image(decorX, decorY, 'dec0')
+                    this.add.image(decorX, decorY, `${terrain}_dec1`)
                 } else if (r < 0.9) {
-                    this.add.image(decorX, decorY, 'dec1')
+                    this.add.image(decorX, decorY, `${terrain}_dec2`)
                 } else {
-                    this.add.image(decorX, decorY, 'dec2')
+                    this.add.image(decorX, decorY, `${terrain}_dec3`)
                 }
             }
         }
@@ -115,7 +117,7 @@ function step(){
     let tile = collapse.call(this, x, y);
     propagate.call(this, x, y);
     this.resolvedGrid[x][y] = true;
-    this.add.image((x * TILE_SIZE) + TILE_SIZE/2, (y * TILE_SIZE) + TILE_SIZE/2, tile);
+    this.add.image((x * TILE_SIZE) + TILE_SIZE/2, (y * TILE_SIZE) + TILE_SIZE/2, `${terrain}${tile}`);
     if (this.grid[x][y].length === 0) {
     console.error("Contradiction at", x, y);
     this.scene.restart();
@@ -123,8 +125,17 @@ function step(){
 }
 
 var game = new Phaser.Game(config);
-let grassTextures = []; // store grass
-let decorTextures = [];
+let grassTextures = [];
+let dirtTextures = [];
+let rockyTextures = [];
+let sandTextures = [];
+let snowyTextures = [];
+let decorGrassTextures = [];
+let decorDirtTextures = [];
+let decorRockyTextures = [];
+let decorSandTextures = [];
+let decorSnowyTextures = [];
+let terrain = Phaser.Utils.Array.GetRandom(["grass", "dirt", "rocky", "sand", "snowy"]);
 
 function preload ()
 {
@@ -135,15 +146,66 @@ function preload ()
         grassTextures.push(name);
         this.load.image(name, `./PNG/grass_tiles/grass${i}.png`);
     }
+    // Loading dirt tiles
+    this.dirtTiles = []
+    for(let i = 0; i <= 16; i++){
+        const name = `dirt${i}`;
+        dirtTextures.push(name);
+        this.load.image(name, `./PNG/dirt_tiles/dirt${i}.png`);
+    }
+    // Loading rocky tiles
+    this.rockyTiles = []
+    for(let i = 0; i <= 16; i++){
+        const name = `rocky${i}`;
+        rockyTextures.push(name);
+        this.load.image(name, `./PNG/rocky_tiles/rocky${i}.png`);
+    }
+    // Loading sand tiles
+    this.sandTiles = []
+    for(let i = 0; i <= 16; i++){
+        const name = `sand${i}`;
+        sandTextures.push(name);
+        this.load.image(name, `./PNG/sand_tiles/sand${i}.png`);
+    }
+    // Loading snowy tiles
+    this.snowyTiles = []
+    for(let i = 0; i <= 16; i++){
+        const name = `snowy${i}`;
+        snowyTextures.push(name);
+        this.load.image(name, `./PNG/snowy_tiles/snowy${i}.png`);
+    }
 
     // Loading decor
     this.decorGrass = [];
     for (let i = 0; i <= 3; i++) {
-        const name = `dec${i}`;
-        decorTextures.push(name);
+        const name = `grass_dec${i}`;
+        decorGrassTextures.push(name);
         this.load.image(name, `./PNG/decor_grass/dec${i}.png`);
-        }
-
+    }
+    this.decorDirt = [];
+    for (let i = 0; i <= 3; i++) {
+        const name = `dirt_dec${i}`;
+        decorDirtTextures.push(name);
+        this.load.image(name, `./PNG/decor_dirt/dec${i}.png`);
+    }
+    this.decorRocky = [];
+    for (let i = 0; i <= 3; i++) {
+        const name = `rocky_dec${i}`;
+        decorRockyTextures.push(name);
+        this.load.image(name, `./PNG/decor_rocky/dec${i}.png`);
+    }
+    this.decorSand = [];
+    for (let i = 0; i <= 3; i++) {
+        const name = `sand_dec${i}`;
+        decorSandTextures.push(name);
+        this.load.image(name, `./PNG/decor_sand/dec${i}.png`);
+    }
+    this.decorSnowy = [];
+    for (let i = 0; i <= 3; i++) {
+        const name = `snowy_dec${i}`;
+        decorSnowyTextures.push(name);
+        this.load.image(name, `./PNG/decor_snowy/dec${i}.png`);
+    }
     // Load water
     this.load.image('water', './PNG/water3.png')
 }
@@ -158,43 +220,43 @@ function create ()
 
     // Rules
     this.sockets = {
-        grass0:  { up: "AA", right: "AA", down: "BA", left: "AB" },
-        grass1:  { up: "BB", right: "BA", down: "BA", left: "BB" },
-        grass2:  { up: "BB", right: "BA", down: "AA", left: "BA" },
-        grass3:  { up: "BB", right: "BB", down: "AB", left: "BA" },
-        grass4:  { up: "AA", right: "AB", down: "AB", left: "AA" },
-        grass5:  { up: "BA", right: "AA", down: "BA", left: "BB" },
-        grass6:  { up: "AA", right: "AA", down: "AA", left: "AA" },
-        grass7:  { up: "AB", right: "BB", down: "AB", left: "AA" },
-        grass8:  { up: "AB", right: "BA", down: "AA", left: "AA" },
-        grass9:  { up: "BA", right: "AA", down: "AA", left: "BA" },
-        grass10: { up: "BA", right: "AB", down: "BB", left: "BB" },
-        grass11: { up: "AA", right: "AB", down: "BB", left: "AB" },
-        grass12: { up: "AB", right: "BB", down: "BB", left: "AB" },
-        grass13: { up: "BA", right: "AB", down: "BB", left: "BB" },
-        grass14: { up: "AA", right: "AB", down: "BB", left: "AB" },
-        grass15: { up: "AB", right: "BB", down: "BB", left: "AB" },
-        grass16: { up: "BB", right: "BB", down: "BB", left: "BB" }
+        0:  { up: "AA", right: "AA", down: "BA", left: "AB" },
+        1:  { up: "BB", right: "BA", down: "BA", left: "BB" },
+        2:  { up: "BB", right: "BA", down: "AA", left: "BA" },
+        3:  { up: "BB", right: "BB", down: "AB", left: "BA" },
+        4:  { up: "AA", right: "AB", down: "AB", left: "AA" },
+        5:  { up: "BA", right: "AA", down: "BA", left: "BB" },
+        6:  { up: "AA", right: "AA", down: "AA", left: "AA" },
+        7:  { up: "AB", right: "BB", down: "AB", left: "AA" },
+        8:  { up: "AB", right: "BA", down: "AA", left: "AA" },
+        9:  { up: "BA", right: "AA", down: "AA", left: "BA" },
+        10: { up: "BA", right: "AB", down: "BB", left: "BB" },
+        11: { up: "AA", right: "AB", down: "BB", left: "AB" },
+        12: { up: "AB", right: "BB", down: "BB", left: "AB" },
+        13: { up: "BA", right: "AB", down: "BB", left: "BB" },
+        14: { up: "AA", right: "AB", down: "BB", left: "AB" },
+        15: { up: "AB", right: "BB", down: "BB", left: "AB" },
+        16: { up: "BB", right: "BB", down: "BB", left: "BB" }
     };
 
     this.weights = {
-        grass0: 3,
-        grass1: 3,
-        grass2: 8,
-        grass3: 3,
-        grass4: 3,
-        grass5: 8,
-        grass6: 50,
-        grass7: 8,
-        grass8: 3,
-        grass9: 3,
-        grass10: 3,
-        grass11: 8,
-        grass12: 3,
-        grass13: 3,
-        grass14: 8,
-        grass15: 3,
-        grass16: 150,
+        0: 3,
+        1: 3,
+        2: 8,
+        3: 3,
+        4: 3,
+        5: 8,
+        6: 50,
+        7: 8,
+        8: 3,
+        9: 3,
+        10: 3,
+        11: 8,
+        12: 3,
+        13: 3,
+        14: 8,
+        15: 3,
+        16: 150,
     };
 
     this.rules = {};
@@ -207,7 +269,7 @@ function create ()
     for (let t1 of tileKeys) {
         for (let t2 of tileKeys) {
         
-            let index2 = parseInt(t2.replace("grass", ""));
+            let index2 = parseInt(t2);
 
            // If Tile 1's UP socket matches Tile 2's DOWN socket
             if (this.sockets[t1].up === this.sockets[t2].down) {
@@ -260,6 +322,7 @@ function create ()
 function update ()
 {
     if(Phaser.Input.Keyboard.JustDown(this.regenKey)){
+        terrain = Phaser.Utils.Array.GetRandom(["grass", "dirt", "rocky", "sand", "snowy"]);
         this.scene.restart();
     }
 }
